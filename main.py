aedabd7d8294ec9cf94c04e749208646d0615761
T-otor <vuettwiller@ecowan.fr>
2024-10-15 12:04:22 +0200

    Retour à la normale sans les ID compromis

--- main.py
+++ main.py
@@ -1,20 +1,36 @@
 import sys
 import os
 import time
-
-# Add the path to the sys.path
+import requests
 sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
 
 def request_info():
-    #IP du Systel:
     ip = input('IP :')
-    port = 8000
-    #Quel Systel ? (1 ou 2)
-    print ('Quel Systel ? (1 = Tempo100-IP ou 2 = Syste Raspi ?)')
+    print('Quel Systel ? (1 = Systel SysBOX ou 2 = Systel Tempo100-IP ?)')
     systel = input('Systel :')
-    return ip, port, systel
+    nbrloop = input('Nombre de boucle :')
+    nbrloop = int(nbrloop)
+    return (ip, systel, nbrloop)
 
+def systel_raspi(ip, nbrloop):
+    for i in range(nbrloop):
+        url = 'http://' + ip + ':8000/api/testPOCSAG'
+        payload = {'id': 0}
+        response = requests.put(url, json=payload)
+        print(response.status_code)
+        print(response.text)
+        if response.status_code == 200:
+            print('Test réussi')
+        else:
+            print('Test échoué')
+            sys.exit(1)
+        time.sleep(27)
 
+def main():
+    ip, systel, nbrloop = request_info()
+    print('IP :', ip)
+    print('Systel :', systel)
+    if systel == '1':
+        systel_raspi(ip, nbrloop)
 if __name__ == '__main__':
-    request_info()
-    
+    main()
